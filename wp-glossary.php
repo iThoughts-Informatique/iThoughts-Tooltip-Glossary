b9a7fcbacc6d6b3980655e961362db4b3c94afde
tcbarrett <tcbarrett@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-21 20:09:27 +0000

    v2.4 notes
    git-svn-id: http://plugins.svn.wordpress.org/wp-glossary/trunk@716192 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-glossary.php
+++ wp-glossary.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://wordpress.org/extend/plugins/wp-glossary/
  * Description: Build a glossary of terms and link your post content to it.
  * Author: TCBarrett
- * Version: 2.3.3
+ * Version: 2.4
  * Author URI: http://www.tcbarrett.com/
  * Text Domain: wp-glossary
  * Domain Path: /lang/
